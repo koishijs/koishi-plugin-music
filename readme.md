90cfff84912cf7d523985961becc920bbd3e70b6
Shigma <1700011071@pku.edu.cn>
2022-02-25 04:36:05 +0800

    add implementation

--- readme.md
+++ readme.md
@@ -2,4 +2,25 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-music?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-music)
 
-Share Music in Koishi
+使用 Koishi 分享音乐。目前支持的平台有：qq, netease。
+
+## 配置项
+
+### platform
+
+- 类型: `string`
+- 默认值: `'qq'`
+
+默认的点歌平台。
+
+### showWarning
+
+- 类型: `boolean`
+
+点歌失败时是否发送提示。
+
+## 指令：music
+
+- 基本语法：`music <keyword>`
+- 选项列表：
+  - -p, --platform \<platform> 点歌平台
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,73 @@
-import { Context } from 'koishi'
+import { Context, Schema, segment } from 'koishi'
+
+export type Platform = 'netease' | 'qq'
+
+export interface Config {
+  showWarning?: boolean
+  platform?: Platform
+}
+
+export const Config: Schema<Config> = Schema.object({
+  showWarning: Schema.boolean().default(false).description('点歌失败时是否发送提示。'),
+  platform: Schema.union<Platform>(['netease', 'qq']).default('qq').description('默认的点歌平台。'),
+})
+
+interface Result {
+  type: string
+  id: string
+}
+
+const platforms: Record<Platform, (this: Context, keyword: string) => Promise<Result>> = {
+  async netease(keyword) {
+    const data = await this.http.get('http://music.163.com/api/cloudsearch/pc', {
+      params: { s: keyword, type: 1, offset: 0, limit: 5 },
+    })
+    if (data.code !== 200) return
+    return {
+      type: '163',
+      id: data.result.songs[0].id,
+    }
+  },
+  async qq(keyword) {
+    const data = await this.http.get('https://c.y.qq.com/soso/fcgi-bin/client_search_cp', {
+      params: { p: 1, n: 5, w: keyword, format: 'json' },
+    })
+    if (data.code) return
+    return {
+      type: 'qq',
+      id: data.data.song.list[0].songid,
+    }
+  },
+}
 
 export const name = 'music'
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, config: Config) {
+  const { showWarning, platform } = config
+
+  ctx.command('music <name:text>', '点歌')
+    // typescript cannot infer type from string templates
+    .option('platform', `-p <platform>  点歌平台，目前支持 qq, netease，默认为 ${platform}`, { type: Object.keys(platforms) })
+    .alias('点歌')
+    .shortcut('来一首', { fuzzy: true })
+    .shortcut('点一首', { fuzzy: true })
+    .shortcut('整一首', { fuzzy: true })
+    .action(async ({ options }, keyword) => {
+      if (!options.platform) options.platform = platform
+      const search = platforms[options.platform]
+      if (!search) {
+        return `目前不支持平台 ${options.platform}。`
+      }
+
+      if (!keyword) return '请输入歌曲相关信息。'
+
+      try {
+        const result = await search.call(ctx, keyword)
+        if (typeof result === 'object') {
+          return segment('music', result)
+        }
+      } catch {}
+
+      if (showWarning) return '点歌失败，请尝试更换平台。'
+    })
 }
