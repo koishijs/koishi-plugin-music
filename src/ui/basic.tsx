3b3ea1f67067392650cb2ca43d593a1eea07124a
TimeBather <open-source@timebather.cn>
2022-12-28 15:20:14 +0800

    Fix : incorrect cut length of the basic mode

--- src/ui/basic.tsx
+++ src/ui/basic.tsx
@@ -5,7 +5,7 @@ export function list(results:Result[]){
     <p>请输入序号以进行选择,输入其他文本视为退出:</p>
     {results.map((t,i)=><>
       <p>{i+1}.<text content={t.name}/></p>
-      <p> - <text content={t.artist.slice(0,10)+(t.artist.length>10?'...':'')}/></p>
+      <p> - <text content={t.artist.slice(0,15)+(t.artist.length>15?'...':'')}/></p>
       </>)}
   </>;
 }
